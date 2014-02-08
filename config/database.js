46c7ee9640f1a48f74aff219c3ef3d0101ebe116
ibarreto <iabarreto11@gmail.com>
2014-02-08 15:45:18 -0700

    scope and files

---
+++ config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+	url : 'mongodb://projectbox:projectbox1@ds027789.mongolab.com:27789/projectbox'
+}
--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log("hello world");
+// server.js for ProjectBox
+
+var express = require('express');
+var app = express(); 
+var port = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var database = require('./config/database'); 
+
+mongoose.connect(database.url);
+
+app.configure(function() {
+	app.use(express.static('./public'));
+	app.use(express.logger('dev'));
+	app.use(express.json());
+	app.use(express.urlencoded());
+	app.use(express.methodOverride());
+});		
+
+require('./app/routes.js')(app);
+
+app.listen(port);
